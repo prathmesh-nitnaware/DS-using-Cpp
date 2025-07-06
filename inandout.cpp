82958030e849c41a14edb546a0fcd66415f0cb4d
Prathmesh Nitnaware <166001818+prathmesh-nitnaware@users.noreply.github.com>
2025-07-06 19:39:39 +0530

    Add files via upload

---
+++ inandout.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    int userAge;
+    std::cout << "Enter your age: ";
+    std::cin >> userAge;
+    std::cout << "You are " << userAge << " years old." << std::endl;
+    return 0;
+}
